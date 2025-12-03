181d9010124b47d6676b21d9bda8bf35a8ffec10
nguyenhaoquang <nguyenhaoquang2004@gmail.com>
2025-12-03 17:59:31 +0700

    create card-adjustment-section inventory

--- src/app/navigation/AuthNavigator.tsx
+++ src/app/navigation/AuthNavigator.tsx
@@ -3,11 +3,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthStackParamList } from './types';
 import { IntroScreen } from '@/features/onboarding/screens/IntroScreen';
 import OnboardingScreen from '@/features/onboarding/screens/OnboardingScreen';
-import CreatePasswordScreen from '@/features/auth/screens/CreatePasswordScreen';
-import RegisterSuccessScreen from '@/features/auth/screens/RegisterSuccessScreen';
 import AuthScreen from '@/features/auth/screens/AuthScreen';
 import VerifyOtpScreen from '@/features/auth/screens/VerifyOtpScreen';
-
+import CreatePasswordScreen from '@/features/auth/screens/CreatePasswordScreen';
+import RegisterSuccessScreen from '@/features/auth/screens/RegisterSuccessScreen';
 // import TestScreen from '@/features/test/screens/TestScreen';
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
