794298a598399cb9b862c4534ec0ba6361df7d78
*wkj8o9dGFd <nshp.ky@vand.vn>
2025-12-22 20:29:07 +0700

    feat: Configure ESLint for TypeScript and React Hooks, and adjust `no-unused-vars` and `no-console` rules.

--- eslint.config.js
+++ eslint.config.js
@@ -34,16 +34,32 @@ module.exports = [
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
+            parser: require('@typescript-eslint/parser'),
             parserOptions: {
                 ecmaFeatures: {
                     jsx: true,
                 },
+                project: null, // Disable type-aware linting for performance
             },
+        },
+        plugins: {
+            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+            'react-hooks': require('eslint-plugin-react-hooks'),
         },
         rules: {
             // Basic rules - sẽ được extend từ @react-native/eslint-config khi có
-            'no-unused-vars': 'warn',
-            'no-console': 'warn',
+            'no-unused-vars': 'off', // Turn off base rule
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+            'no-console': 'off', // Allow console in mobile development
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
         },
     },
 ];
