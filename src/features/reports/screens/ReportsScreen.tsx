e08ed1cbc89f3f4a87aaedb13ab724471875657e
Hydra Coder <ngodat.it213@gmail.com>
2025-12-23 21:10:49 +0700

    fix: resolve conflict

--- src/features/reports/screens/ReportsScreen.tsx
+++ src/features/reports/screens/ReportsScreen.tsx
@@ -172,8 +172,6 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     contentContainer: {
         paddingBottom: 20,
