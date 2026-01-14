45c24290b2d9acdae4c8ba987d04e14d019861d5
nguyenhaoquang <nguyenhaoquang2004@gmail.com>
2026-01-14 20:41:16 +0700

    move file to the correct location

---
+++ src/features/menu/components/delete-account/DeleteAccountWarningStep.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native';
+import { colors, spacing, borderRadius } from '@/styles';
+
+interface Props {
+    style?: StyleProp<ViewStyle>;
+}
+
+export const DeleteAccountWarningBox: React.FC<Props> = ({ style }) => {
+    return (
+        <View style={[styles.warningBox, style]}>
+            <Text style={styles.warningText}>
+                Nếu xoá tài khoản, toàn bộ dữ liệu sẽ bị xoá và không thể khôi phục lại, ngay cả khi
+                bạn đăng ký lại tài khoản mới bằng số điện thoại hiện tại.
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    warningBox: {
+        backgroundColor: colors.volcano[100],
+        borderWidth: 1,
+        borderColor: colors.volcano[400],
+        borderRadius: borderRadius.md,
+        padding: spacing.sm,
+        width: '100%',
+    },
+    warningText: {
+        fontSize: 14,
+        lineHeight: 22,
+        color: colors.text,
+        paddingHorizontal: spacing.md,
+        paddingVertical: spacing.sm,
+    },
+});
