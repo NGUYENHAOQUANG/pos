2c0ac7775b51f56a0b5d8baaa95d47ff42347bc0
*wkj8o9dGFd <nshp.ky@vand.vn>
2026-01-20 15:09:05 +0700

    refactor: standardize app name to Mebieco and update application versions across platforms.

--- ios/MebiOne/AppDelegate.swift
+++ ios/MebiOne/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MebiEco",
+      withModuleName: "Mebieco",
       in: window,
       launchOptions: launchOptions
     )
