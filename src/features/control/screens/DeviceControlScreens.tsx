65fd55926b654ab2f4d27e7e02f67484683f9346
Nguyễn Minh Quân <quannguyen19092003@gmail.com>
2026-03-17 08:44:29 +0700

    fix zoneid

--- src/features/control/screens/DeviceControlScreens.tsx
+++ src/features/control/screens/DeviceControlScreens.tsx
@@ -74,7 +74,7 @@ export const DeviceControlScreens = () => {
         isLoading: isLoadingPonds,
         refetch,
         isRefetching,
-    } = useAllPondsByZone(selectedZoneId);
+    } = useAllPondsByZone(selectedZoneId ?? '');
 
     // Ensure valid array
     const farmPonds = useMemo(() => {
